fbac0dc756aaee7369880a2185d4f87145faf233
Mazen Moazzam <mm5622@drexel.edu>
2025-06-21 22:54:55 -0400

    Added postgres configurations with backend integrations and tests

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,37 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-CORS(app)  # Allow requests from React dev server
+CORS(app)
 
-@app.route("/api/videos")
-def get_videos():
-    return jsonify([
-        {"id": 1, "title": "How to cook rice", "url": "video1.mp4"},
-        {"id": 2, "title": "Learn Flask in 10 minutes", "url": "video2.mp4"}
-    ])
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Nezam1234%23%25@localhost:5434/streamnest_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f"<Video {self.title}>"
+
+
+
+
+@app.route("/api/testdb")
+def test_db():
+    if not Video.query.filter_by(title="Test Video").first():
+        test_video = Video(title="Test Video", url="test_video.mp4")
+        db.session.add(test_video)
+        db.session.commit()
+    videos = Video.query.all()
+    return jsonify([{"id": v.id, "title": v.title, "url": v.url} for v in videos])
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
